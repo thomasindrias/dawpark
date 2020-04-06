0f2f7e33557531ceb934a980a0d6cf8327295e5a
Filip <filkl010@student.liu.se>
2020-04-06 17:53:58 +0200

    Added some basic middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,14 @@
 import express, { Response } from "express";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
+
+// Setup middlewares
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (_, res: Response) => {
   res.send("Dawpark!");
