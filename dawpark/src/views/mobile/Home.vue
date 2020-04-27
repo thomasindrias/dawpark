a27e69dbd5a9666d563c9efbdb8028fc9ce914c4
thomasindrias <thoin216@student.liu.se>
2020-04-27 23:24:26 +0200

    Result page added

--- dawpark/src/views/mobile/Home.vue
+++ dawpark/src/views/mobile/Home.vue
@@ -1,21 +1,38 @@
 <template>
-  <map-box
-  :accessToken="accessToken"
-  :mapStyle="mapStyle"
-  :parkings="parkings"
-  @result="searchHandler"
-  :mobile="true"
-  :proximity="proximity"
-  ></map-box>
+  <div class="home" id="home">
+    <div class="map-page">
+      <div class="map">
+        <map-box
+        :accessToken="accessToken"
+        :mapStyle="mapStyle"
+        :parkings="parkings"
+        @result="searchHandler"
+        :mobile="true"
+        :proximity="proximity"
+        ></map-box>
+      </div>
+    </div>
+    <div class="mobile-page">
+      <div class="mobile-container" id="mobile">
+        <mobile-page
+        v-if="parkings.searchResult !== null"
+        :parkings="parkings"
+        @proximity="searchHandler"/>
+      </div>
+      </div>
+  </div>
 </template>
 
 <script>
+import VueScrollTo from 'vue-scrollto';
 import MapBox from '@/components/MapBox.vue';
+import MobilePage from '@/components/MobilePage.vue';
 import EventService from '@/services/EventService';
 
 export default {
   components: {
     MapBox,
+    MobilePage,
   },
   data() {
     return {
@@ -26,6 +43,13 @@ export default {
         data: [],
       },
       proximity: '10000',
+      scrollOptions: {
+        container: '#home',
+        easing: 'ease-in',
+        offset: -60,
+        force: true,
+        cancelable: true,
+      },
     };
   },
   methods: {
@@ -45,10 +69,55 @@ export default {
         .catch((err) => {
           console.log(err);
         });
+
+      VueScrollTo.scrollTo('#mobile', 500, this.scrollOptions);
     },
   },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.home {
+  height: 100%;
+  width: 100%;
+  overflow-y: auto;
+}
+
+.map-page {
+  // flex: 1;
+  height: 100vh;
+}
+
+.map {
+  height: 100%;
+  width: 100%;
+}
+
+.mobile-page {
+  // flex: 1;
+  position: relative;
+  z-index: 999;
+}
+
+.mobile-container {
+  // flex: 1;
+  position: absolute;
+  top:-30px;
+  width: 100%;
+  z-index: 999;
+}
+
+.column {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  flex-wrap: wrap;
+}
+
+.row {
+  display: flex;
+  flex-basis: 100%;
+  flex-direction: row;
+  flex: 1;
+}
 </style>
